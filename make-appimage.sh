8b8ea123fd1d7ddaf6403d0a5100d394bc918784
domesya <163451587+domesya@users.noreply.github.com>
2026-05-22 16:11:56 +0200

    adding bulky here (?)

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,12 +10,19 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/16x16/apps/nemo.png
 export DESKTOP=/usr/share/applications/nemo.desktop
+export DEPLOY_PYTHON=1
+export PATH_MAPPING='/usr/share/bulky:${SHARUN_DIR}/share/bulky'
 
-export DEPLOY_GTK=1
-export DEPLOY_GDK=1
+# /usr/bin/bulky is a bash script that just executes bulky
+# there is no point in that, just symlink the python script directly lol
+ln -sf /usr/lib/bulky/bulky.py /usr/bin/bulky
+
+# allow relocating locales
+sed -i -e 's|LOCALE_DIR =.*|LOCALE_DIR = os.environ.get("TEXTDOMAINDIR", "/usr/share/locale")|' /usr/lib/bulky/bulky.py
 
 # Deploy dependencies
-quick-sharun /usr/bin/nemo
+quick-sharun /usr/bin/nemo \
+  /usr/bin/bulky /usr/lib/libgtk-3.so*
 
 # Additional changes can be done in between here
 
