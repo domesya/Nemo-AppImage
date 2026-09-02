c708e068dc09fcbf0568be0dd76fd4397692ee19
domesya <163451587+domesya@users.noreply.github.com>
2026-09-02 17:15:10 +0200

    Enhance quick-sharun command with more paths
    Expanded the quick-sharun command to include additional directories for deployment.

--- make-appimage.sh
+++ make-appimage.sh
@@ -24,8 +24,13 @@ ln -sf /usr/lib/bulky/bulky.py /usr/bin/bulky
 sed -i -e 's|LOCALE_DIR =.*|LOCALE_DIR = os.environ.get("TEXTDOMAINDIR", "/usr/share/locale")|' /usr/lib/bulky/bulky.py
 
 # Deploy dependencies
-quick-sharun /usr/bin/nemo /usr/bin/bulky /usr/lib/libgtk-3.so*
-
+quick-sharun \
+  /usr/bin/nemo  \
+  /usr/bin/bulky \
+  /usr/lib/libgtk-3.so* \
+  /usr/lib/nemo/extensions-3.0/  \
+  /usr/share/nemo-python/extensions/
+  
 # Additional changes can be done in between here
 
 # Turn AppDir into AppImage
