9d90c4c247d1e23c3bb67a463b0d7f6c86b830c1
domesya <163451587+domesya@users.noreply.github.com>
2026-09-02 17:08:30 +0200

    Add patchelf to package dependencies installation

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm nemo nemo-fileroller nemo-share nemo-terminal nemo-repairer nemo-compare nemo-audio-tab nemo-image-converter nemo-python
+pacman -Syu --noconfirm nemo nemo-fileroller nemo-share nemo-terminal nemo-repairer nemo-compare nemo-audio-tab nemo-image-converter nemo-python patchelf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
